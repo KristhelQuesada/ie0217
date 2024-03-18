689e2e51ed9b23f12342093c8ccaf853c6b9020b
KristhelQuesada <kristhelquesada24@gmail.com>
2024-03-17 23:52:20 -0600

    Actualizando archivo.cpp (cuando se actualizaba el README)
    Signed-off-by: KristhelQuesada <kristhelquesada24@gmail.com>

--- Previos/Previo1/archivo.cpp
+++ Previos/Previo1/archivo.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World \n";
+    std::cout << "Hello World but modified to test git diff \n";
+    std::cout << "Trying the git stash command \n";
     return 0;
 }
