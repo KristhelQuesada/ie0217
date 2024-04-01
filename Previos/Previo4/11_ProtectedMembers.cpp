ae417976401c7fbff24564f59a348593bece6827
KristhelQuesada <kristhelquesada24@gmail.com>
2024-04-01 08:08:01 -0600

    Documentacion de los archivos 9-10 para el previo4
    Signed-off-by: KristhelQuesada <kristhelquesada24@gmail.com>

--- Previos/Previo4/11_ProtectedMembers.cpp
+++ Previos/Previo4/11_ProtectedMembers.cpp
@@ -2,14 +2,29 @@
 #include <string>
 using namespace std;
 
+/*
+DESCRIPCION
+En este programa se continua con el estudio de la herencia, mas en este
+caso se observara el efecto de los especificadores de acceso en los miem-
+bros, todo con base en la diapositiva 16 de la presentacion.
+
+En este caso se define un objeto clase perro que tiene los mismos metodos
+que animal y de los derivados, pero al ejecutar run() vemos la sobreescritura
+donde se ejecuta el de la clase derivada.
+*/
+
+// Base class
 class Animal {
     private:
+        // Private member
         string color;
 
     protected:
+        // Protected member
         string type;
 
     public:
+        // Public methods
         void run() {cout << "I can run! -- Base!" << endl;}
         void eat() {cout << "I can eat!" << endl;}
         void sleep() {cout << "I can sleep!" << endl;}
@@ -17,18 +32,29 @@ class Animal {
         string getColor() {return color;}
 };
 
+// Derived class from base class Animal
 class Dog : public Animal {
     public:
+        // Overwriting function run() from base class
+        /*
+        Hasta cierto punto aca se presencia el efecto del polimorfismo
+        es decir, un mismo metodo puede tomar distintas formas, y por
+        tanto es solo una manera mas de hacer herencia.
+        */
         void run() {cout << "I can run! -- Derived!" << endl;}
+
+        // 4 methods of its own
+
+        // its access to type is possible because type is a protected
+        // member, so it can be accessed from a derived class
         void setType(string tp) {type = tp;}
-        
+
         void displayInfo(string c) {
             cout << "I am a " << type;
             cout << " and my color is " << c << endl;
         }
-
-        // void setColor(string clr) {color = clr;}
-
+        
+        // void setColor(string clr) {color = clr;} but this can't be done
         void bark() {cout << "I can bark!" << endl;}
 };
 
--- Previos/Previo4/8_OverloadOfOperator.cpp
+++ Previos/Previo4/8_OverloadOfOperator.cpp
@@ -1,18 +1,61 @@
 #include <iostream>
 using namespace std;
 
+/*
+DESCRIPCION
+Este programa funciona para introducir el concepto de sobrecarga de operadores
+en C++ segun la diapositiva 12 de la presentacion. Dicho esto, dicha sobrecarga
+permite definir el comportamiento de los operadores.
+
+Por ejemplo, operadores como +, -, *, etc, entonces consideremos la clase Per-
+sona donde tenemos persona1 y persona2, si nos dicen que sumemos ambos objetos
+ahi nos preguntamos how? porque no sabemos que implica sumar ambos objetos como
+si dijeramos sumar 2+2.
+
+Entonces nosotros como desarrolladores definimos que queremos que el compilador
+interprete al darle esa instruccion (persona1+persona2).
+*/
+
 class Fraccion{
+    // Como quedan definidas estas variables?
     int numerador, denominador;
 
     public:
+        // Definicion del constructor donde se asocia que los
+        // atributos numerador y denominador seran inicializa-
+        // dos por los valores que se pasen por parametros.
         Fraccion(int n, int d) : numerador(n), denominador(d) {}
 
+        /*
+        Luego se define la sintaxis para la modificacion del operador
+        que queremos sobrecargar, es decir, aqui queremos darle o es-
+        pecificar un resultado especial cuando se utilice el operador
+        "+" cuando se sumen fracciones, es decir, objetos de la clase
+        fraccion.
+
+        Su sintaxis esta dada por:
+
+        <Nombre de la clase> operador<operador> (const <type> &<referencia>)
+        */
+
         Fraccion operator+ (const Fraccion &f) {
+            // En este caso resultado es una variable de tipo Fraccion
+            // donde vamos a colocar el resultado de la suma de la frac
+
+            // el f.denominador o f.numerador son los denominadores o
+            // numeradores de la otra fraccion que nos pasaron por parametro
+            // en la linea 41 mediante la referencia.
+
+            // Se separa por coma ya que primero se define la operacion a
+            // realizar para calcular el numerador y que asi se albergue en
+            // el objeto o variable tipo Fraccion, y luego mismo para el de-
+            // nominador
             Fraccion resultado (
                 numerador*f.denominador + f.numerador*denominador,
                 denominador*f.denominador
             );
 
+            // Luego se retorna la variable resultado tipo Fraccion
             return resultado;
         }
 
@@ -23,11 +66,15 @@ class Fraccion{
 
 
 int main() {
+    // Se instancian dos fracciones, con sus valores de numerador
+    // y denomiador ya inicializadas.
     Fraccion f1(1,2);
     Fraccion f2(3,4);
 
+    // Luego lo siguiente es posible gracias a la sobrecarga de operadores
     Fraccion f3 = f1 + f2;
 
+    // Se imprime el resultado
     f3.imprimir();
 
     return 0;
