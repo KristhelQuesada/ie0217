abaede580ef68c87cf9b2524c444734652a0308e
KristhelQuesada <kristhelquesada24@gmail.com>
2024-04-09 17:40:55 -0600

    Finalizacion del Previo5, se agregaron los comentarios a los archivos restantes con base en el video y se actualiza el README
    Signed-off-by: KristhelQuesada <kristhelquesada24@gmail.com>

--- Previos/Previo5/6_Queue_IMPT.cpp
+++ Previos/Previo5/6_Queue_IMPT.cpp
@@ -3,8 +3,15 @@ using namespace std;
 
 /*
 PROGRAM FOR QUEUE IMPLEMENTATION IN C++
-Basado en la diapositiva 16 de la presentacion. 
-
+Basado en la diapositiva 16 de la presentacion donde se explica el
+funcionamiento de la estructura queue o cola. Funciona de tal manera
+que el primer elemento en entrar es el primer elemento en salir. En
+esta estructura se tendran dos punteros donde ambos iniciaran en la
+misma posicion cuando no haya nada o un unico elemento, y conforme
+se vaya llenando la cola el puntero rear (trasero) comenzara a moverse
+y conforme se eliminen elementos, el puntero front (frontal) tambien
+comenzara a moverse hacia el rear. Una vez se hayan eliminado todos
+los elementos entonces ambos punteros regresan a la posicion inicial.
 */
 
 // Defines and global variables
@@ -16,88 +23,96 @@ class Queue {
         int items[SIZE], front, rear;
 
     public:
+        // Constructor que nos inicializa las variables en -1
         Queue() {
             front = -1;
             rear = -1;
         }
 
 
-    // Function to check if the queue is full
-    bool isFull() {
-        if (front == 0 && rear == SIZE-1) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    // Function to check if the queue is empty
-    bool isEmpty() {
-        if (front == -1) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    // Function to insert element into the queue
-    void enQueue(int element) {
-        if (isFull()) {
-            cout << "\nQueue is full";
-        } else {
-            if (front == -1) front = 0;
-
-            rear++;
-            items[rear] = element;
-
-            cout << endl
-                << "Inserted " << element << endl;
-        }
-    }
-
-    // Function to delete element into the queue
-    void deQueue() {
-        int element;
-
-        if (isEmpty()) {
-            cout << "\nQueue is empty";
-            return (-1);
-        } else {
-            element = items[front];
-
-            if (front >= rear) {
-                front = -1;
-                rear = -1;
-            } // Q has only one element, so we reset the queue after deleting it
-            else {
-                front++;
-            }
-
-            cout << "\nDeleted -> " << element << endl;
-
-            return element;
-        }
-    }
-
-    // Function to display the info of the queue
-    void display() {
-        // Function to display elements of queue
-        int i;
-
-        if (isEmpty()) {
-            cout << "\nEmpty queue" << end;
-        } else {
-            cout << "\nFront index -> " << front;
-            cout << "\nItems -> ";
-
-            for (i=0, i <= rear; i++) {
-                cout << items[i] << endl;
-                    << "Rear index -> " << rear << endl;
+        // Function to check if the queue is full
+        bool isFull() {
+            // Chequea si el puntero frontal esta en la primera posicion
+            // (no se han eliminado elementos) y si el trasero esta al final
+            // donde el -1 era porque las posiciones empiezan en cero.
+            if (front == 0 && rear == SIZE-1) {
+                return true;
+            } else {
+                return false;
             }
         }
-    }
 
-};
+        // Function to check if the queue is empty
+        bool isEmpty() {
+            if (front == -1) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        // Function to insert element into the queue
+        void enQueue(int element) {
+            // pregunta si esta lleno
+            if (isFull()) {
+                cout << "\nQueue is full";
+
+                // si no esta lleno entonces permite el ingreso de elementos
+            } else {
+                if (front == -1) front = 0;
+
+                rear++;
+                items[rear] = element;
+
+                cout << endl
+                    << "Inserted " << element;
+            }
+        }
+
+        // Function to delete element from the queue
+        int deQueue() {
+            int element;
+
+            if (isEmpty()) {
+                cout << "\nQueue is empty" << endl;
+                return -1;
+            } else {
+                element = items[front];
+
+                if (front >= rear) {
+                    front = -1;
+                    rear = -1;
+                } // Q has only one element, so we reset the queue after deleting it
+                else {
+                    front++;
+                }
+
+                cout << "\nDeleted -> " << element << endl;
+
+                return element;
+            }
+        }
+
+        // Function to display the info of the queue
+        void display() {
+            // Function to display elements of queue
+            int i;
+
+            if (isEmpty()) {
+                cout << "\nEmpty queue" << endl;
+            } else {
+                cout << "\nFront index -> " << front;
+                cout << "\nItems -> ";
+
+                for (i=0; i <= rear; i++) {
+                    cout << items[i];
+                }
+
+                cout << "\nRear index -> " << rear << endl;
+            }
+        }
+
+};  
 
 
 // Driver code
@@ -119,6 +134,7 @@ int main() {
     q.enQueue(6);
 
     // Display info by far
+    cout << endl;
     q.display();
 
     // deQueue removes element entered first (1 in our case)
--- Previos/Previo5/7_LinkedList_IMPT.cpp
+++ Previos/Previo5/7_LinkedList_IMPT.cpp
@@ -4,29 +4,39 @@ using namespace std;
 
 /*
 PROGRAM FOR LINKED LIST IMPLEMENTATION IN C++
-Basado en la diapositiva 17 de la presentacion. 
-
+Basado en la diapositiva 17 de la presentacion. Se busca ampliar el concepto
+de listas enlazadas como estructura de datos lineal, la cual consta de una
+serie de nodos enlazados, donde cada nodo almacena los datos y la direccion
+del siguiente nodo. Tambien se mencionan otro tipos de listas como las doble-
+mente enlazadas y las listas circulares.
 */
 
 // Creating a node
 class Node {
     public:
-        int value;
-        Node* next;
+        int value; // dato almacenado (valor)
+        Node* next; // nodo puntero, es decir, aca se guardara la direccion
+                    //del siguiente nodo
 };
 
 
 // Driver code
 int main() {
     // Declaring a pointer that points to the 1st node (head) of the linked list.
-    // Therefore head is a variable that will contain the memory direction of an
+    // Therefore head is a variable that will contain the memory direction of a
     // class Node object.
     Node* head;
 
     // Initializing next nodes as pointer of a class Node object
+    // all of them initialized in null
     Node* one = NULL;
     Node* two = NULL;
     Node* three = NULL;
+
+    // Allocate each node on heap because of 'new'
+    one = new Node();
+    two = new Node();
+    three = new Node();
 
     // Assign values to value atribute for each object
     one->value = 1;
@@ -45,6 +55,13 @@ int main() {
         cout << head->value;
         head = head->next;
     }
+
+    cout << endl;
+
+    delete head;
+    delete one;
+    delete two;
+    delete three;
 
     return 0;
 }
