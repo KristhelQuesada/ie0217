7620c7dcb4c83e789f50a37170e58a9dff7995a0
KristhelQuesada <kristhelquesada24@gmail.com>
2024-05-15 10:23:03 -0600

    Previo 10. En EventosKeyboard, se agrega un boton tal que se observa que no se registra la entrada del teclado al tener este seleccionado
    Signed-off-by: KristhelQuesada <kristhelquesada24@gmail.com>

--- Previos/Previo10/EventosKeyboard/MainFrame.cpp
+++ Previos/Previo10/EventosKeyboard/MainFrame.cpp
@@ -5,14 +5,19 @@
 // Constructor de la clase
 MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) {
     wxPanel* panel = new wxPanel(this);
-    //wxButton* btn1 = new wxButton(panel, wxID_ANY, "Button", wxPoint(300, 250), wxSize(200, 100));
-    //wxButton* btn2 = new wxButton(panel, wxID_ANY, "Button", wxPoint(300, 250), wxSize(200, 100));
+    wxButton* btn1 = new wxButton(panel, wxID_ANY, "Button 1", wxPoint(300, 250), wxSize(200, 100));
+    wxButton* btn2 = new wxButton(panel, wxID_ANY, "Button 2", wxPoint(300, 250), wxSize(200, 100));
 
-    CreateStatusBar();
-    
-
+    /*
+    Solo asi lo que sucede es que se detecta la entrada del teclado cuando no
+    estamos seleccionando el boton
+    */
     panel->Bind(wxEVT_KEY_DOWN, &MainFrame::OnKeyEvent, this);
-    //button->Bind(wxEVT_MOTION, &MainFrame::OnMouseEvent, this);
+    btn1->Bind(wxEVT_KEY_DOWN, &MainFrame::OnKeyEvent, this);
+    //button->Bind(wxEVT_KEY_DOWN, &MainFrame::OnKeyEvent, this);
+    
+    
+    CreateStatusBar();
 }
 
 // Funciones miembro privadas
